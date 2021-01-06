076eddbba8f9f0c5fd4383eb4b9a0315b0c2c9ff
Wenchy Dutreuil <wcdutreuil@gmail.com>
2021-01-05 23:59:33 -0500

    Add files via upload

---
+++ snapcheckencryptionalternate.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class snapcheckencryptionalternate {
+	public static final UnaryOperator<String> removeWhiteSpace =
+            str -> str.codePoints()
+                    .boxed()
+                    .filter(code -> !Character.isWhitespace((char)code.intValue()))
+                    .collect(Collector.of(
+                            () -> new String[] {""},
+                            (result, item) -> result[0] += (char) item.intValue(),
+                            (r1, r2) ->  {
+                                r1[0] += r2[0];
+                                return r1; },
+                            r -> r[0]));
+
+
+	public static void main(String[] args)  {
+		try {
+			System.out.println(encrypt(args[0]));
+		} catch (IndexOutOfBoundsException ex) {
+			System.out.println("You did not provide text to be encrypted.");
+		}
+	}
+
+	public static String encrypt(String s) {
+		String encrypted = s.transform(removeWhiteSpace);
+		double lengthRoot = Math.sqrt(encrypted.length());
+		int rows = (int)Math.floor(lengthRoot);
+		int cols = (int)Math.ceil(lengthRoot);
+
+		StringBuilder encryptedString = new StringBuilder();
+		for (int i = 0; i < cols; i++) {
+			for (int j = i; j < encrypted.length(); j+= cols) {
+				encryptedString.append(encrypted.charAt(j));
+			}
+			encryptedString.append(" ");
+		}
+
+		return encryptedString.toString();
+	}
+}
